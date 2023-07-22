194a811cd487e1a7c4e53a00ea1a1ff639e674db
Luísa Menezes | Lume <83649500+clair-de-lume@users.noreply.github.com>
2023-07-22 20:47:02 -0300

    update main menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,83 @@
 <template>
-  <div id="canvas">
-    <div id="deep-space" />
-    <div id="space-field">
-      <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
-
-      <SpaceObject class="asteroid" :data="asteroid" resolution="2"
-                  :key="asteroid.center"
-                  v-for="asteroid in spaceField.asteroids" />
-
-      <SpaceObject class="missile" :data="missile" resolution="2"
-                  :key="missile.center"
-                  v-for="missile in spaceField.missiles" />
-
-      <SpaceObject class="explosion" :data="explosion" resolution="2"
-                  :key="explosion.center"
-                  v-for="explosion in spaceField.explosions" />
+  <div class="background-container">
+    <div class="background-gif"></div>
+    <div class="content-container">
+      <div class="title">
+        <h1 class="gradient-title">GALAXY RAIDERS</h1>
+      </div>
+      <div class="buttons-container">
+        <NuxtLink to="play" class="button button1" style="color: white;">PLAY</NuxtLink>
+        <NuxtLink to="leaderboard" class="button button2" style="color: white;">LEADERBOARD</NuxtLink>
+        <NuxtLink to="exit" class="button button3" style="color: white;">QUIT</NuxtLink>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-const {
-  data: spaceField,
-  refresh: updateSpaceField
-} = await $get("/space-field");
-
-onMounted(() => {
-  window.addEventListener("keydown", async (event) => {
-    const keyToCommand = {
-      "ArrowUp": "MOVE_SHIP_UP",
-      "ArrowDown": "MOVE_SHIP_DOWN",
-      "ArrowRight": "MOVE_SHIP_RIGHT",
-      "ArrowLeft": "MOVE_SHIP_LEFT",
-      "Space": "LAUNCH_MISSILE",
-      "Escape": "PAUSE_GAME",
-    };
-
-    const command = keyToCommand[event.code];
-
-    // Ignore if invalid key was pressed
-    if (command === undefined) return;
-
-    console.log(`Triggering command: ${command}`);
-    await $post("/ship/commands", { command })
-  });
-
-  window.setInterval(updateSpaceField, 1000);
-})
-</script>
-
 <style>
-#canvas {
-  height: calc(100vh - 4rem);
-  width: calc(100vw - 4rem);
+.title {
+  text-align: center;
+  margin: 20px auto;
+}
 
-  padding: 2rem;
+.gradient-title {
+  background: linear-gradient(45deg, #ff007f, #29abe2);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: white;
+  font-size: 72px;
+  font-family: "Orbitron", sans-serif;
+}
 
-  background-color: #36bbf5;
-  overflow: hidden;
-
-  position: relative;
-
+.buttons-container {
   display: flex;
+  position: page;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  height: 100%;
 }
 
-@keyframes slide {
-  0% {
-    transform: translate(1px);
-  }
-  50% {
-    transform: translate(-1px);
-  }
-  100% {
-    transform: translate(1px);
-  }
+.button {
+  padding: 15px 40px;
+  font-size: 24px;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-decoration: none;
+  margin: 10px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+  box-shadow: 0 0 0 2px white;
 }
 
-#deep-space {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  background-image: url("~/assets/space.png");
-  background-origin: content-box;
-  animation: slide 3s linear infinite;
-
-  position: absolute;
-  z-index: 0;
+.background-gif {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: url("https://i.pinimg.com/originals/e1/b9/c5/e1b9c5f696d6f136c3eca1b26ac8c846.gif");
+  background-size: cover;
+  background-repeat: no-repeat;
+  z-index: -1;
+  opacity: 1;
 }
 
-#space-field {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
+.background-container {
   position: relative;
+  width: 100%;
+  height: 100%;
 }
 
-.spaceship {
-  background-image: url("~/assets/spaceship.png");
+.content-container {
+  position: relative;
+  z-index: 1;
 }
 
-.asteroid {
-  background-image: url("~/assets/asteroid.png");
-}
-
-.missile {
-  background-image: url("~/assets/missile.png");
-}
-
-.explosion {
-  background-image: url("~/assets/explosion.png");
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
